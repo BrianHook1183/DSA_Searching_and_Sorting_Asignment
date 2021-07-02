69e7d0bd4ba88ead2032ae643481d4a26bb34419
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-02 02:09:19 -0400

    passes tests

--- src/searchByName.js
+++ src/searchByName.js
@@ -4,6 +4,52 @@
  * @param {string} lastName
  * @param {array} customers
  */
-function searchByName(firstName, lastName, customers) {}
+
+function compare(element, target) {
+  if (element < target) {
+    return 1;
+  }
+  if (element > target) {
+    return -1;
+  }
+  if (element === target) {
+    return 0;
+  }
+}
+
+function searchByName(firstName, lastName, customers) {
+  if (Array.isArray(customers)) {
+    let lowerIndex = 0;
+    let upperIndex = customers.length - 1;
+    while (lowerIndex <= upperIndex) {
+      const index = Math.floor((upperIndex + lowerIndex) / 2);
+      const comparisonLastName = compare(customers[index].lastName, lastName);
+      const comparisonFirstName = compare(customers[index].firstName, firstName);
+
+      // move index until last name matches
+      if (comparisonLastName > 0) {
+        lowerIndex = index + 1;
+      }
+      if (comparisonLastName < 0) {
+        upperIndex = index - 1;
+      }
+      if (comparisonLastName === 0) {
+        // Only checks first name once last name finds a match
+        if (comparisonFirstName > 0) {
+          lowerIndex = index + 1;
+        }
+        if (comparisonFirstName < 0) {
+          upperIndex = index - 1;
+        }
+        // when first and last name both match, index is returned
+        if (comparisonFirstName === 0) {
+          return index;
+        }
+      }
+    }
+    return -1;
+  }
+  return -1;
+}
 
 module.exports = searchByName;
