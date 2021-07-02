72a94ad6fd6281937ad5fd9fac2251ffb56ceb25
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-02 01:37:10 -0400

    nameSort() passes tests

--- src/sortByName.js
+++ src/sortByName.js
@@ -4,6 +4,34 @@ const sort = require("./sort");
  * Sort the array of customers by first and last name
  * @param {array} customers
  */
-function sortByName(customers) {}
+function compare(left, right) {
+    if (left < right) {
+        return -1;
+    }
+    if (left > right) {
+        return 1;
+    }
+    if (left === right) {
+        return 0;
+    }
+}
 
-module.exports = sortByName;
+function nameSort(customers) {
+
+    let names = customers.map((customer) => customer.lastName + customer.firstName);
+
+    let sorted = sort(compare, names);
+    customers = [];
+
+    sorted.forEach((name) => {
+        let lastName = name.slice(0, 1);
+        let firstName = name.slice(1, name.length);
+
+        let newObj = { firstName, lastName };
+
+        customers.push(newObj);
+    })
+    return customers;
+}
+
+module.exports = nameSort;
